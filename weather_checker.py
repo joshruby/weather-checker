c4c4346515ba02920935be1efaa34619df318433
Joshua Ruby <joshuaruby@Joshuas-MacBook-Pro.local>
2020-04-28 18:24:07 -0700

    Initial commit

--- weather_checker.py
+++ weather_checker.py
@@ -1,11 +1,19 @@
+# mapbox weather-checker token:
+mapbox_token = 'pk.eyJ1IjoianJydWJ5IiwiYSI6ImNrOWtrMDU3czF2dTkzZG53Nmw2NDdneTMifQ.zzXEhr0Z1biR2pydOFco8A'
+# This token is configured for this script in ./streamlit/config.toml
+# It's being initialized as a var here as well to use with plotly
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 import pydeck as pdk
 import requests
 from streamlit import caching
+# import altair as alt
+import plotly.graph_objects as go
+import plotly.express as px
 
-st.subheader('Testing the "requests" library')
+st.header('Weather Checker')
 
 zip_code = st.text_input('Zip Code', value='', key=None, type='default')
 
@@ -14,14 +22,22 @@ if zip_code:
 
     r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=' + zip_code + ',us&appid=5c3d72cdc2b3e6fc1c06ddc0004a713b')
 
+    # Debug only
+    # st.subheader('Raw request')
     # st.write(r.json())
 
+    city_name = r.json()['name']
     location = r.json()['coord']
     weather_k = temp_k = r.json()['main']
-    temp_k = r.json()['main']['temp']
+    # temp_k = r.json()['main']['temp']
+    temp_f = (weather_k['temp'] - 273.15) * 1.8 + 32
+    feels_like_f = (weather_k['feels_like'] - 273.15) * 1.8 + 32
+    high_f = (weather_k['temp_max'] - 273.15) * 1.8 + 32
+    low_f = (weather_k['temp_min'] - 273.15) * 1.8 + 32
+    humidity = weather_k['humidity']
+    conditions = r.json()['weather'][0]['main']
 
     # st.write(location)
-
     lat = location['lat']
     lon = location['lon']
 
@@ -34,35 +50,49 @@ if zip_code:
 
     loc_df = pd.DataFrame(loc_arr, columns=['lat', 'lon'])
     # st.write(loc_df)
-    st.map(loc_df, zoom=11)
 
-    # st.pydeck_chart(pdk.Deck(
-    #     map_style='mapbox://styles/mapbox/light-v9',
-    #     view_state=pdk.ViewState(
-    #         latitude=lat,
-    #         longitude=lon,
-    #         zoom=11,
-    #         pitch=50,
-    #     ),
-    #     layers= pdk.Layer(
-    #             'ScatterplotLayer',
-    #             data=loc_df,
-    #             get_position='[lon, lat]',
-    #             get_color='[200, 30, 0, 160]',
-    #             get_radius=200,
-    #     ),
-    # ))
+    # st.map(loc_df, zoom=11)
 
-    # test_map_df = pd.DataFrame(np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    #                         columns=['lat', 'lon'])
-    # st.write(test_map_df)
-    # st.write(test_map_df.iloc[1,1])
-    # st.map(test_map_df[:1], zoom=11)
+    # Plotly map (that actually updates!)
+    fig = go.Figure(go.Scattermapbox(
+        lat=loc_df['lat'],
+        lon=loc_df['lon'],
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=9
+        ),
+        text=[city_name],
+    ))
 
-    st.write('Current weather in ' + zip_code + ' [K]: ', weather_k)
+    fig.update_layout(
+        autosize=False,
+        margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=0,
+            pad=0
+        ),
+        hovermode='closest',
+        mapbox=dict(
+            accesstoken=mapbox_token,
+            bearing=0,
+            center=dict(
+                lat=lat,
+                lon=lon
+            ),
+            pitch=0,
+            zoom=9
+        ),
+    )
 
-    temp_f = (temp_k - 273.15) * 1.8 + 32
+    st.plotly_chart(fig, use_container_width=False)
 
-    st.write('Current temperature in ' + zip_code + ' [F]: ', float('%.2f' % temp_f))
+    st.subheader('Current weather in ' + city_name)
 
-# st.button('Refresh')
+    # st.write('Conditions: ', conditions)
+    st.write('Temperature: ', float('%.2f' % temp_f))
+    st.write('Feels Like: ', float('%.2f' % feels_like_f))
+    st.write('Humidity: ', humidity)
+    # st.write('Today\'s high: ', float('%.2f' % high_f))
+    # st.write('Today\'s low: ', float('%.2f' % low_f))
